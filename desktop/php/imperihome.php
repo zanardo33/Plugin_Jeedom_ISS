7386205961f6aa28b5ec3d27584243a3afbff805
zanardo33 <vincent.zanardo@free.fr>
2019-06-27 14:30:59 +0200

    Vincent
    Changement de l'ID et du nom

---
+++ desktop/php/imperihome.php
@@ -0,0 +1,156 @@
+<?php
+
+if (!isConnect('admin')) {
+  throw new Exception('{{401 - Accès non autorisé}}');
+}
+sendVarToJS('eqType', 'imperihome');
+$eqLogics = eqLogic::byType('imperihome');
+
+?>
+
+<div class="row row-overflow">
+  <div class="col-lg-2 col-sm-3 col-sm-4">
+    <div class="bs-sidebar">
+      <ul id="ul_eqLogic" class="nav nav-list bs-sidenav">
+        <a class="btn btn-default eqLogicAction" style="width : 100%;margin-top : 5px;margin-bottom: 5px;" data-action="add"><i class="fas fa-plus-circle"></i> {{Ajouter un équipement}}</a>
+        <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+        <?php
+        foreach ($eqLogics as $eqLogic) {
+          echo '<li class="cursor li_eqLogic" data-eqLogic_id="' . $eqLogic->getId() . '"><a>' . $eqLogic->getHumanName(true) . '</a></li>';
+        }
+        ?>
+      </ul>
+    </div>
+  </div>
+
+  <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+    <legend><i class="fas fa-table"></i>  {{Mes Imperihome}}
+    </legend>
+    <div class="eqLogicThumbnailContainer">
+      <div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+        <center>
+          <i class="fas fa-plus-circle" style="font-size : 7em;color:#00979c;"></i>
+        </center>
+        <span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>Ajouter</center></span>
+      </div>
+      <?php
+      $dir = dirname(__FILE__) . '/../../doc/images/';
+      $files = scandir($dir);
+      foreach ($eqLogics as $eqLogic) {
+        $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
+        echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff ; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;' . $opacity . '" >';
+        echo "<center>";
+        $test = $eqLogic->getConfiguration('type') . '.png';
+        if (in_array($test, $files)) {
+          $path = $eqLogic->getConfiguration('type');
+        } else {
+          $path = 'imperihome_icon';
+        }
+        echo '<img src="plugins/imperihome/doc/images/' . $path . '.png" height="105" width="95" />';
+        echo "</center>";
+        echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+        echo '</div>';
+      }
+      ?>
+    </div>
+  </div>
+
+
+  <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
+
+    <a class="btn btn-success eqLogicAction pull-right" data-action="save"><i class="fas fa-check-circle"></i> {{Sauvegarder}}</a>
+    <a class="btn btn-danger eqLogicAction pull-right" data-action="remove"><i class="fas fa-minus-circle"></i> {{Supprimer}}</a>
+
+    <ul class="nav nav-tabs" role="tablist">
+      <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fas fa-tachometer"></i> {{Equipement}}</a></li>
+      <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fas fa-list-alt"></i> {{Commandes}}</a></li>
+    </ul>
+
+    <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
+      <div role="tabpanel" class="tab-pane active" id="eqlogictab">
+        <form class="form-horizontal">
+          <fieldset>
+            <legend><i class="fas fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i>  {{Général}}
+              <i class="fas fa-cogs eqLogicAction pull-right cursor" data-action="configure"></i>
+            </legend>
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Equipement Imperihome}}</label>
+              <div class="col-sm-3">
+                <input type="text" class="eqLogicAttr form-control" data-l1key="id" style="display : none;" />
+                <input type="text" class="eqLogicAttr form-control" data-l1key="name" placeholder="{{Nom de l'équipement Imperihome}}"/>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label" >{{Objet parent}}</label>
+              <div class="col-sm-3">
+                <select class="form-control eqLogicAttr" data-l1key="object_id">
+                  <option value="">{{Aucun}}</option>
+                  <?php
+                  foreach (object::all() as $object) {
+                    echo '<option value="' . $object->getId() . '">' . $object->getName() . '</option>';
+                  }
+                  ?>
+                </select>
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Catégorie}}</label>
+              <div class="col-sm-8">
+                <?php
+                foreach (jeedom::getConfiguration('eqLogic:category') as $key => $value) {
+                  echo '<label class="checkbox-inline">';
+                  echo '<input type="checkbox" class="eqLogicAttr" data-l1key="category" data-l2key="' . $key . '" />' . $value['name'];
+                  echo '</label>';
+                }
+                ?>
+
+              </div>
+            </div>
+            <div class="form-group">
+              <label class="col-sm-3 control-label" ></label>
+              <div class="col-sm-8">
+                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
+                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Commentaire}}</label>
+              <div class="col-sm-3">
+                <textarea class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="commentaire" ></textarea>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <label class="col-sm-3 control-label">{{Adresse IP}}</label>
+              <div class="col-sm-3">
+                <input class="eqLogicAttr form-control" data-l1key="configuration" data-l2key="addr" type="text" placeholder="{{saisir ip du imperihome}}">
+              </div>
+            </div>
+
+        </fieldset>
+      </form>
+    </div>
+  <div role="tabpanel" class="tab-pane" id="commandtab">
+<table id="table_cmd" class="table table-bordered table-condensed">
+  <thead>
+    <tr>
+      <th style="width: 50px;">#</th>
+      <th style="width: 300px;">{{Nom}}</th>
+      <th style="width: 200px;">{{Paramètres}}</th>
+      <th style="width: 100px;"></th>
+    </tr>
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
+
+</div>
+
+</div>
+</div>
+</div>
+
+<?php include_file('desktop', 'imperihome', 'js', 'imperihome'); ?>
+<?php include_file('core', 'plugin.template', 'js'); ?>
